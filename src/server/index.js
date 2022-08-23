3d32f82fca81b3e6285bae095b7cc8d9d01aa235
Saeed99Madi <saed.zooro@gmail.com>
2022-08-23 15:36:23 +0300

    create file structure related to #1

--- src/server/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-
-const port = app.get("port");
-const { NODE_ENV } = process.env;
-
-app.listen(port, () => {
-  console.log(`server running at http//localhost:${port} with ${NODE_ENV}`);
-});
