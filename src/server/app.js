e463cbdaa6a5239bebcdd76c6955611a0848cfcf
nader shakshak <nadershak149@gmail.com>
2022-08-23 23:20:23 +0300

    create routes for pages relats #11

--- src/server/app.js
+++ src/server/app.js
@@ -15,6 +15,37 @@ app.use(compression());
 
 app.set('port', process.env.PORT)
 
+// routes
+
+const path = require('path');
+
+const {serverError, clientErrors} = require('../middleware/errors')
+
+
+
+// app.use(express.static(path.join(__dirname,'..', 'public')));
+app.use(express.static(path.join(__dirname, '..','..', 'public')));  // index.html
+
+app.get('/menu', (req,res)=>{
+    res.sendFile(path.join(__dirname,'..' ,'..','public','menu.html')); //menu.html
+});
+app.get('/admin', (req,res)=>{
+    res.sendFile(path.join(__dirname,'..' ,'..','public','admin.html')); //admin.html
+});
+
+//500.html  Error page handle
+app.get('/500', (req,res)=>{
+    throw new Error('Error')
+});
+
+
+
+
+
+app.use(clientErrors);
+app.use(serverError);
+
+
 
 module.exports = app;
 
