2823a7912e3131dd1bbdac330aefd532b55978a9
Saeed99Madi <saed.zooro@gmail.com>
2022-08-24 10:53:04 +0300

    cerate new folder contain control panel pages for categories and meals related to #23

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -4,16 +4,7 @@ const { Pool } = require("pg");
 if (!process.env.DB_URL) {
   throw new Error("DB_URL must be specified");
 }
-if (process.env.NODE_ENV === "production") {
-  module.exports = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  module.exports = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl: false,
-  });
-}
+module.exports = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: false,
+});
